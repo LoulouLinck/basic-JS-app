5b064947a411af9c444cc76fdae419ea029ab12c
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-06-28 19:26:17 +0200

    deleted object titles

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,24 +1,24 @@
 let pokemonList = [
 
-  pokemon1 = {
+ {
          name: 'Balbusaur',
          height: 0.7,
          types: ['grass', 'poison'],
-    };
+    },
 
-  pokemon2 = {
+ {
          name: 'Ninetales',
          height: 1.1,
          types: ['Fire'],
 
-    };
+    },
 
-  pokemon3 = {
+  {
          name: 'Mienshao',
          height: 1.4,
          types: ['Fighting'],
 
-    };
+    },
 ];
 
 // loop to go through all objects in PokemonList
