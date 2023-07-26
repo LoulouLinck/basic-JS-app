28e3c6a7a049789efa57795c26c32bfce3eea25c
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-07-26 22:41:29 +0200

    Updated w/ bootstrap
    utility classes

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,61 +1,60 @@
-let pokemonRepository = (function() { //wraps pokemonList arrray in IIFE
+let pokemonRepository = (function () {
   let pokemonList = [];
-  let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; // API to which app will make requests 
-  let modalContainer = document.querySelector('#modal-container');
-
+  let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; // API to which app will make 
+   let modalContainer = document.querySelector('#modal-container');
+  
   function hideModal() {
-    modalContainer.classList.remove('is-visible');
-  }
-
+  modalContainer.classList.remove('is-visible');
+}
   window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();  
-    }
-  });
-
-  modalContainer.addEventListener('click', (e) => {
-    // listener also triggered by clicks on modal: child of modalContainer.
-    let target = e.target; 
-    if (target === modalContainer) { //specifies modal should close only if click on overlay = turquoise part: modalContainer. 
-      hideModal();
-    }
-  });
-
-  function showModal(title, text, img) {
-    
-       // Clears all existing modal content
-       modalContainer.innerHTML = '';
-    
-       let modal = document.createElement('div');
-       modal.classList.add('modal');
-     
-       // Adds the new modal content
-       let closeButtonElement = document.createElement('button');
-       closeButtonElement.classList.add('modal-close');
-       closeButtonElement.innerText = 'X Close';
-     
-       let titleElement = document.createElement('h1');
-       titleElement.innerText = title;
-     
-       let contentElement = document.createElement('p');
-       contentElement.innerText = text;
-
-       let imageElement = document.createElement("img");
-       imageElement.setAttribute("src", img);
-       imageElement.setAttribute("width", "50%");
-       imageElement.setAttribute("height", "50%");
-       imageElement.setAttribute("alt", "Pokemon image");
-      
-       modalContainer.appendChild(modal);
-       modal.appendChild(closeButtonElement);
-       modal.appendChild(titleElement);
-       modal.appendChild(contentElement)
-       modal.appendChild(imageElement);
-       closeButtonElement.addEventListener('click', hideModal);
-
-       modalContainer.classList.add('is-visible');
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();  
   }
+});
+  
+  modalContainer.addEventListener('click', (e) => {
+  // listener also triggered by clicks on modal: child of modalContainer.
+  let target = e.target; 
+  if (target === modalContainer) { //specifies modal should close only if click on overlay = turquoise part: modalContainer. 
+    hideModal();
+  }
+});
+  
+   function showModal(title, text, img) {
+      // Clears all existing modal content
+      modalContainer.innerHTML = '';
+    
+      let modal = document.createElement('div');
+      modal.classList.add('modal');
+    
+      // Adds the new modal content
+      let closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'X Close';
+    
+      let titleElement = document.createElement('h1');
+      titleElement.innerText = title;
+    
+      let contentElement = document.createElement('p');
+      contentElement.innerText = text;
 
+      let imageElement = document.createElement("img");
+      imageElement.setAttribute("src", img);
+      imageElement.setAttribute("width", "50%");
+      imageElement.setAttribute("height", "50%");
+      imageElement.setAttribute("alt", "Pokemon image");
+
+     
+      modalContainer.appendChild(modal);
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      modal.appendChild(imageElement);
+     closeButtonElement.addEventListener('click', hideModal);
+
+      modalContainer.classList.add('is-visible');
+    }
+  
 function getAll() { // returns all pokemon in pokemonList
   return pokemonList;
 }
@@ -68,16 +67,19 @@ function add(pokemon) { //adds pokemon object to array pokemonList
       pokemonList.push(pokemon);
     } else {
       console.log('pokemon is not correct');
-  }
-  
+  } 
 }
 
-function addListItem(pokemon){
+function addListItem(pokemon){ // add a list of buttons to the array
   let pokemonUl = document.querySelector('.pokemon-list'); // var assigned to <ul = "pokemon-list"> 
   let listItem = document.createElement('li'); // creation of a list item
+  listItem.classList.add('list-group-item'); //adds bootstrap
+  listItem.classList.add('list-group-item-action'); //adds bootstrap hover styling
   let button = document.createElement('button'); // creation of a button
   button.innerText = pokemon.name; // set text in button element as element's names returned from pokemonRepository's array: pokemonList through IIFE access: getAll()  
-  button.classList.add('pokeButton'); // set new class attribute to our button
+  button.classList.add('pokeButton');// set new class attribute to our button
+  button.classList.add('btn');  //adds bootstrap  utility classes 
+  button.classList.add('btn-light'); //adds bootstrap  utility classes styling
   listItem.appendChild(button); // append button
   pokemonUl.appendChild(listItem); // append li
 
@@ -87,12 +89,12 @@ function addListItem(pokemon){
 }
 
 function showDetails(pokemon){ // logs pokemon object 
-  pokemonRepository.loadDetails(pokemon).then(function(){
-    showModal(pokemon.name, " Height: " + pokemon.height, pokemon.imageUrl);
-    console.log(pokemon);  
+pokemonRepository.loadDetails(pokemon).then(function(){   
+  showModal(pokemon.name, " Height: " + pokemon.height, pokemon.imageUrl);
+  console.log(pokemon);  
   });
 }
-
+  
 function loadList(){
   return fetch(apiURL).then(function(response){
     return response.json();
